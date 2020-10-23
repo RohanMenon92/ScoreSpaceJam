28569c6313f08504bb99adbc52ac391323705a5b
rohanm92 <randommenon@gmail.com>
2020-10-24 03:38:00 +0530

    Initial commit

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConstants
+{
+    public enum PlayerStates {
+
+    }
+
+    public enum ProjectileTypes
+    {
+
+    }
+
+    public enum EnemyTypes
+    {
+
+    }
+}
